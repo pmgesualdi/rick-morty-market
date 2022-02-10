1d7ae3c810dc740646b1e7f4f6c7dbba09cb8999
pmgesualdi <pmgesualdi@gmail.com>
2022-02-10 18:34:21 -0300

    Update List component - Implement LoadingCard

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,19 +1,44 @@
+import { useMemo } from 'react';
 import { useRecoilState } from 'recoil';
-import { characterState } from '../../state/marketplace';
+import { characterState } from '../../state/recoil';
 import Card from '../Card';
 import { Character } from '../Container/interfaces';
+import LoadingCard from '../LoadingCard';
 import styles from './styles.module.scss'
 
-const List = () => {
+interface ListProps {
+  showFavorites: boolean
+}
+
+const List = ({ showFavorites }: ListProps) => {
   const [characters] = useRecoilState(characterState)
+  const loadingPlaceholder = useMemo(() => {
+    return Array(20).fill(<LoadingCard />)
+  }, [])
+  const favorites: Array<Character> = useMemo(() => {
+    if (!characters) return []
+
+    return characters.filter((char: Character) => char.favorite)
+  }, [characters])
+
+  if (!characters || !characters.length) return (
+    <section className={styles.list}>
+      {loadingPlaceholder}
+    </section>
+  )
+
+  const cards: Array<Character> = showFavorites ? favorites : characters 
 
   return (
     <section className={styles.list}>
-      {characters.map((character: Character, i) => (
+      {cards.map((character: Character, i) => (
         <li key={`${character.name}-${i}`}>
           <Card {...character} />
         </li>
       ))}
+      {!cards.length && (
+        <div className={styles.noFavorites}>You have to select some favorite characters first!</div>
+      )}
     </section>
   )
 }
