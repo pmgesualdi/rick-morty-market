2094959584e9590f4b44888d0e816313b854664c
pmgesualdi <pmgesualdi@gmail.com>
2022-02-10 18:34:33 -0300

    Update Card component and style

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,28 +1,28 @@
 import { useSetRecoilState } from "recoil";
 import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 import { Character } from "../Container/interfaces";
-import { characterState } from "../../state/marketplace";
+import { characterState } from "../../state/recoil";
 import styles from "./styles.module.scss";
 
-const Card = ({ name, image, favorite }: Character) => {
+const Card = ({ id, name, image, favorite }: Character) => {
   const setCharactersState = useSetRecoilState(characterState);
+  const updateFavoriteCharacters = (characters: Array<Character>) => {
+    return characters
+      .map((char: Character) => {
+        if (char.id !== id) return char
+
+        return { ...char, favorite: !favorite }
+      }) as any
+    }
 
   return (
     <div className={styles.card}>
       <img src={image} alt={name} />
       <div className={styles.info}>
-        <p>{name}</p>
+        <div>{name}</div>
         <button
           className={styles.favorite}
-          onClick={() =>
-            setCharactersState(
-              (characters) =>
-                [
-                  ...characters.filter((char: Character) => char.name !== name),
-                  { name, image, favorite: true },
-                ] as any
-            )
-          }
+          onClick={() => setCharactersState(updateFavoriteCharacters)}
         >
           {favorite ? (
             <AiFillStar className={styles.star} />
